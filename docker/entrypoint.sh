008d22ba03867abd9351e0a0cb1279fe4a6db1ac
Mülken Burak LALE <mulkenburak@gmail.com>
2025-10-02 01:42:39 +0300

    Laravel and Docker initialized

---
+++ docker/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap/cache
+chmod -R 775 storage bootstrap/cache
+
+cd /var/www/html
+
+echo "Install application package  (npm and composer)"
+npm install
+composer install
+
+php artisan optimize:clear
+
+echo "Run migration for database"
+php artisan migrate
+
+composer dump-autoload
+
+echo "Cache, config, view cleaning and optimize..."
+php artisan cache:clear
+php artisan config:clear
+php artisan view:clear
+php artisan config:cache
+
+php artisan optimize:clear
+
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+
+php artisan db:seed
+echo "Application ready."
