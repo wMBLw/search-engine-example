ae2dcf8b30090b078c704e98d920ba1f3b8fbab3
Mülken Burak LALE <mulkenburak@gmail.com>
2025-10-02 02:55:38 +0300

    Define model local scope for user email

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Builder;
 
 class User extends Authenticatable
 {
@@ -45,5 +46,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function scopeByEmail(Builder $query, string $email): Builder
+    {
+        return $query->where('email', $email);
     }
 }
