db7914ab17563423fee8f610748f68eff27d1efc
Mülken Burak LALE <mulkenburak@gmail.com>
2025-10-02 03:12:01 +0300

    Define SetLocaleFromHeader middleware to Laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append(\App\Http\Middleware\SetTimezoneFromHeader::class);
+        $middleware->append(\App\Http\Middleware\SetLocaleFromHeader::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
 
