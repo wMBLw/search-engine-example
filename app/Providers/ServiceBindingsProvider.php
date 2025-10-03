88e913005e2ff6e5d28059104bbd73a9b6ee1e0c
Mülken Burak LALE <mulkenburak@gmail.com>
2025-10-03 05:06:31 +0300

    Bind SearchService and ScoreCalculator to ServiceBindingsProvider

--- app/Providers/ServiceBindingsProvider.php
+++ app/Providers/ServiceBindingsProvider.php
@@ -7,6 +7,10 @@ use App\Services\Content\ContentSyncService;
 use App\Services\Content\Contracts\ContentSyncServiceInterface;
 use App\Services\Content\Contracts\DistributedLockInterface;
 use App\Services\Content\RedisDistributedLock;
+use App\Services\Search\Contracts\ScoreCalculatorInterface;
+use App\Services\Search\Contracts\SearchServiceInterface;
+use App\Services\Search\ScoreCalculator;
+use App\Services\Search\SearchService;
 use Illuminate\Support\ServiceProvider;
 
 class ServiceBindingsProvider extends ServiceProvider
@@ -16,6 +20,8 @@ class ServiceBindingsProvider extends ServiceProvider
         $this->app->bind(AuthServiceInterface::class, AuthService::class);
         $this->app->bind(DistributedLockInterface::class, RedisDistributedLock::class);
         $this->app->bind(ContentSyncServiceInterface::class, ContentSyncService::class);
+        $this->app->bind(SearchServiceInterface::class, SearchService::class);
+        $this->app->bind(ScoreCalculatorInterface::class, ScoreCalculator::class);
     }
 
     public function boot()
