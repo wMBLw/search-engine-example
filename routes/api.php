2b6ff0f19b9529e8a3b0e4afa036cbfd8d3e22af
Mülken Burak LALE <mulkenburak@gmail.com>
2025-10-03 07:58:53 +0300

    Define dashboard api for frontend

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,6 +12,11 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::group(['prefix' => 'user'], function () {
         Route::get('/', [AuthController::class, 'loggedInUser']);
         Route::get('/logout', [AuthController::class, 'logout']);
+    });
+
+    Route::group(['prefix' => 'search'], function () {
+        Route::get('/', [SearchController::class, 'search']);
+        Route::get('/statistics', [SearchController::class, 'statistics']);
     });
 
 });
