27eba29ec2da9fb7748b518249f793eddde8c29a
Mülken Burak LALE <mulkenburak@gmail.com>
2025-10-03 05:05:45 +0300

    Bind SearchRepository to RepositoryServiceProvider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -5,6 +5,8 @@ use App\Repositories\Auth\AuthRepository;
 use App\Repositories\Auth\AuthRepositoryInterface;
 use App\Services\Content\ContentRepository;
 use App\Services\Content\Contracts\ContentRepositoryInterface;
+use App\Services\Search\Contracts\SearchRepositoryInterface;
+use App\Services\Search\SearchRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);
         $this->app->bind(ContentRepositoryInterface::class, ContentRepository::class);
-
+        $this->app->bind(SearchRepositoryInterface::class, SearchRepository::class);
     }
 
     public function boot()
