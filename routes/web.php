beb8fb6828434fb67ad1c6ef745deb1ae80ae302
Mülken Burak LALE <mulkenburak@gmail.com>
2025-10-03 08:04:49 +0300

    Define route for frontend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'login'])->name('login');
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
