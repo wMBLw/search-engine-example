d07a928746f8193b5814372e5714744b698bff1f
Mülken Burak LALE <mulkenburak@gmail.com>
2025-10-03 06:04:45 +0300

    Create pipeline filter for search api sorting filter

---
+++ app/Filters/SortingFilter.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filters;
+
+use App\Services\Search\SearchCriteriaDTO;
+use Closure;
+use Illuminate\Database\Eloquent\Builder;
+
+class SortingFilter
+{
+    public function __construct(private SearchCriteriaDTO $criteria)
+    {
+    }
+
+    public function handle(Builder $query, Closure $next)
+    {
+        $sortBy = $this->criteria->getSortBy();
+        $direction = $this->criteria->getSortDirection();
+
+        // Rankings other than Score are done at the database layer
+        // The score ranking will be done in SearchService because it is calculated with Strategy Pattern
+        if ($sortBy !== 'score') {
+            $this->applySorting($query, $sortBy, $direction);
+        }
+
+        return $next($query);
+    }
+
+    private function applySorting(Builder $query, string $sortBy, string $direction): void
+    {
+        switch ($sortBy) {
+            case 'views':
+                $query->orderBy('views', $direction);
+                break;
+
+            case 'likes':
+                $query->orderBy('likes', $direction);
+                break;
+
+            case 'published_at':
+                $query->orderBy('published_at', $direction);
+                break;
+
+            case 'title':
+                $query->orderBy('title', $direction);
+                break;
+
+            default:
+                $query->orderBy('published_at', $direction);
+                break;
+        }
+    }
+}
+
