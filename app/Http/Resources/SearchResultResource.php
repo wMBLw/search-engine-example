02e369cad91b846f8ded050ffc4a0a71f0b21692
Mülken Burak LALE <mulkenburak@gmail.com>
2025-10-03 07:20:50 +0300

    Create resource for content and search result

---
+++ app/Http/Resources/SearchResultResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class SearchResultResource extends ResourceCollection
+{
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => ContentResource::collection($this->collection)
+        ];
+    }
+
+    /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @return array<string, mixed>
+     */
+    public function with(Request $request): array
+    {
+        return [
+            'success' => true,
+        ];
+    }
+}
+
