83314ee3bd4a0c0e5dd8a833c0aca0fe8b787100
Mülken Burak LALE <mulkenburak@gmail.com>
2025-10-03 07:07:36 +0300

    Create helper class for score calculator

--- app/Providers/ServiceBindingsProvider.php
+++ app/Providers/ServiceBindingsProvider.php
@@ -7,9 +7,7 @@ use App\Services\Content\ContentSyncService;
 use App\Services\Content\Contracts\ContentSyncServiceInterface;
 use App\Services\Content\Contracts\DistributedLockInterface;
 use App\Services\Content\RedisDistributedLock;
-use App\Services\Search\Contracts\ScoreCalculatorInterface;
 use App\Services\Search\Contracts\SearchServiceInterface;
-use App\Services\Search\ScoreCalculator;
 use App\Services\Search\SearchService;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,7 +19,6 @@ class ServiceBindingsProvider extends ServiceProvider
         $this->app->bind(DistributedLockInterface::class, RedisDistributedLock::class);
         $this->app->bind(ContentSyncServiceInterface::class, ContentSyncService::class);
         $this->app->bind(SearchServiceInterface::class, SearchService::class);
-        $this->app->bind(ScoreCalculatorInterface::class, ScoreCalculator::class);
     }
 
     public function boot()
